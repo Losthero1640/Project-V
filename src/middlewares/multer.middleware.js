ea017a86203e65678df86e43be4e35649ba603a6
Losthero1640 <ayushmanpattanaik001@gmail.com>
2026-06-13 14:22:39 +0530

    fix: dynamically create public/temp directory for multer uploads

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,15 @@
 import multer from "multer";
+import fs from "fs";
+
+const uploadDir = "./public/temp";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -12,3 +19,4 @@ const storage = multer.diskStorage({
 export const upload = multer({
   storage,
 });
+
